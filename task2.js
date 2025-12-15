a614e5d2e99a0fae8f75216d3b6973dedad0db3f
Andrea Martínez <andreamartinezdl1@gmail.com>
2025-12-15 15:28:59 -0600

    SE AGREGO A TASK 2 FIZZBUZZ

--- task2.js
+++ task2.js
@@ -1,5 +1,27 @@
 // Refer to Task 2 in your Instructions to complete this task
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Two!");
-  };
+const prompt = require("prompt-sync")();
+console.log("This is Task Two!");
+const numbers = [];
+
+//llenamos el array hasta 105
+for (let i = 1; i <=105; i++) {
+  //numbers.push(i);
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(i + " FIZZBUZZ");
+  } else if (i % 3 === 0) {
+    console.log(i + " FIZZ");
+  } else if (i % 5 === 0) {
+    console.log(i + " BUZZ");
+  }else if (i % 7 === 0) {
+    console.log(i + " WOOF");
+  } 
+  else {
+    console.log(i);
+  }
+  }
+for (let j = 0; j < numbers.length; j++) {
+  console.log(numbers[j]);
+ }
+
+ 
